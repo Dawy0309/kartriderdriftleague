e17baac2e91820f53916b568dfb26712f8ac5717
Dawy <51861890+Dawy0309@users.noreply.github.com>
2023-05-31 11:46:37 +0800

    Add files via upload

---
+++ dist/data.js
@@ -0,0 +1,294 @@
+var teamSatMatch = "01/00(Tue.) UTC+8 16:00";
+var teamSunMatch = "02/00(Tue.) UTC+8 16:00";
+var teamWedMatch = "03/00(Tue.) UTC+8 16:00";
+var teamSatResult = "04/00(Tue.)";
+var teamSunResult = "05/00(Tue.)";
+var teamWedResult = "06/00(Tue.)";
+
+var indSatMatch = "01/00(Tue.) UTC+8 16:00";
+var indSunMatch = "02/00(Tue.) UTC+8 16:00";
+var indSatResult = "04/00(Tue.)";
+var indSunResult = "05/00(Tue.)";
+
+var Team = [
+  {
+    name:"LSB",
+    img:"Team/liiv sandbox.png"
+  },
+  {
+    name:"SNT",
+    img:"Team/sensation.png"
+  },
+  {
+    name:"FNL",
+    img:"Team/FINALE e-sports.png"
+  },
+  {
+    name:"KDF",
+    img:"Team/kwangdong freecs(r).png"
+  },
+  {
+    name:"ROX",
+    img:"Team/rox.png"
+  },
+  {
+    name:"LSB",
+    img:"Team/liiv sandbox.png"
+  },
+  {
+    name:"LSB",
+    img:"Team/liiv sandbox.png"
+  },
+  {
+    name:"SNT",
+    img:"Team/sensation.png"
+  },
+  {
+    name:"FNL",
+    img:"Team/FINALE e-sports.png"
+  },
+  {
+    name:"KDF",
+    img:"Team/kwangdong freecs(r).png"
+  },
+  {
+    name:"ROX",
+    img:"Team/rox.png"
+  },
+  {
+    name:"LSB",
+    img:"Team/liiv sandbox.png"
+  },
+  {
+    name:"ROX",
+    img:"Team/rox.png"
+  },
+  {
+    name:"LSB",
+    img:"Team/liiv sandbox.png"
+  }
+  
+];
+
+var TeamResult = [
+  {
+    name:"LSB",
+    img:"Team/liiv sandbox.png",
+    score: "2",
+  },
+  {
+    name:"ROX",
+    img:"Team/rox.png",
+    score: "0",
+  },
+  {
+    name:"KDF",
+    img:"Team/kwangdong Freecs(r).png",
+    score: "2",
+  },
+  {
+    name:"PLN",
+    img:"Team/planet.png",
+    score: "0",
+  },
+  {
+    name:"Aura",
+    img:"Team/aura.png",
+    score: "2",
+  },
+  {
+    name:"RBT",
+    img:"Team/reboot.png",
+    score: "1",
+  },
+  {
+    name:"LSB",
+    img:"Team/liiv sandbox.png",
+    score: "2",
+  },
+  {
+    name:"ROX",
+    img:"Team/rox.png",
+    score: "0",
+  },
+  {
+    name:"KDF",
+    img:"Team/kwangdong Freecs(r).png",
+    score: "2",
+  },
+  {
+    name:"PLN",
+    img:"Team/planet.png",
+    score: "0",
+  },
+  {
+    name:"AU",
+    img:"Team/aura.png",
+    score: "2",
+  },
+  {
+    name:"RBT",
+    img:"Team/reboot.png",
+    score: "1",
+  },
+  {
+    name:"AU",
+    img:"Team/aura.png",
+    score: "2",
+  },
+  {
+    name:"RBT",
+    img:"Team/reboot.png",
+    score: "1",
+  }
+];
+
+var Player = [
+    {
+      name: "NEAL",
+      img:"Player/NEAL.png",
+    },
+    {
+      name: "HyunSu",
+      img:"Player/HyunSu.png",
+    },
+    {
+      name: "Cool",
+      img:"Player/Cool.png",
+    },
+    {
+      name: "World",
+      img:"Player/World.png",
+    },
+    {
+      name: "SPEAR",
+      img:"Player/SPEAR.png",
+    },
+    {
+      name: "Spell",
+      img:"Player/Spell.png",
+    },
+    {
+      name: "DDing",
+      img:"Player/DDing.png",
+    },
+    {
+      name: "JiMin",
+      img:"Player/JiMin.png",
+    },
+    {
+      name: "NEAL",
+      img:"Player/NEAL.png",
+    },
+    {
+      name: "HyunSu",
+      img:"Player/HyunSu.png",
+    },
+    {
+      name: "Cool",
+      img:"Player/Cool.png",
+    },
+    {
+      name: "World",
+      img:"Player/World.png",
+    },
+    {
+      name: "SPEAR",
+      img:"Player/SPEAR.png",
+    },
+    {
+      name: "Spell",
+      img:"Player/Spell.png",
+    },
+    {
+      name: "DDing",
+      img:"Player/DDing.png",
+    },
+    {
+      name: "JiMin",
+      img:"Player/JiMin.png",
+    }
+  ];
+
+var PlayerResult = [
+  {
+    name: "NEAL",
+    score: "50",
+    img:"Player/NEAL.png",
+  },
+  {
+    name: "HyunSu",
+    score: "45",
+    img:"Player/HyunSu.png",
+  },
+  {
+    name: "Cool",
+    score: "40",
+    img:"Player/Cool.png",
+  },
+  {
+    name: "World",
+    score: "35",
+    img:"Player/World.png",
+  },
+  {
+    name: "SPEAR",
+    score: "30",
+    img:"Player/SPEAR.png",
+  },
+  {
+    name: "Spell",
+    score: "25",
+    img:"Player/Spell.png",
+  },
+  {
+    name: "DDing",
+    score: "20",
+    img:"Player/DDing.png",
+  },
+  {
+    name: "JiMin",
+    score: "15",
+    img:"Player/JiMin.png",
+  },
+  {
+    name: "NEAL",
+    score: "50",
+    img:"Player/NEAL.png",
+  },
+  {
+    name: "HyunSu",
+    score: "45",
+    img:"Player/HyunSu.png",
+  },
+  {
+    name: "Cool",
+    score: "40",
+    img:"Player/Cool.png",
+  },
+  {
+    name: "World",
+    score: "35",
+    img:"Player/World.png",
+  },
+  {
+    name: "SPEAR",
+    score: "30",
+    img:"Player/SPEAR.png",
+  },
+  {
+    name: "Spell",
+    score: "25",
+    img:"Player/Spell.png",
+  },
+  {
+    name: "DDing",
+    score: "20",
+    img:"Player/DDing.png",
+  },
+  {
+    name: "JiMin",
+    score: "15",
+    img:"Player/JiMin.png",
+  }
+];
---
+++ dist/main.js
@@ -0,0 +1,29 @@
+var num;
+
+for(num = 0; num < 14; num++){
+    document.getElementById('Team'+ num).textContent = Team[num].name;
+    document.getElementById('Team' + num + '_img').src = Team[num].img;
+    document.getElementById('TeamResult'+ num).textContent = TeamResult[num].name;
+    document.getElementById('TeamResult' + num + '_score').textContent = TeamResult[num].score;
+    document.getElementById('TeamResult' + num + '_img').src = TeamResult[num].img;
+}
+
+for(num = 0; num < 16; num++){
+    document.getElementById('Player' + num).textContent = Player[num].name;
+    document.getElementById('Player' + num + '_img').src = Player[num].img;
+    document.getElementById('PlayerResult' + num).textContent = PlayerResult[num].name;
+    document.getElementById('PlayerResult' + num + '_score').textContent = PlayerResult[num].score;
+    document.getElementById('PlayerResult' + num + '_img').src = PlayerResult[num].img;
+}
+
+document.getElementById('teamSatMatch').textContent = teamSatMatch;
+document.getElementById('teamSunMatch').textContent = teamSunMatch;
+document.getElementById('teamWedMatch').textContent = teamWedMatch;
+document.getElementById('teamSatResult').textContent = teamSatResult;
+document.getElementById('teamSunResult').textContent = teamSunResult;
+document.getElementById('teamWedResult').textContent = teamWedResult;
+
+document.getElementById('indSatMatch').textContent = indSatMatch;
+document.getElementById('indSunMatch').textContent = indSunMatch;
+document.getElementById('indSatResult').textContent = indSatResult;
+document.getElementById('indSunResult').textContent = indSunResult;
