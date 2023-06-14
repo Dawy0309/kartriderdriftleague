670455e52efc8986779d0dd58a152b32b8ff91ff
Dawy <51861890+Dawy0309@users.noreply.github.com>
2023-06-14 13:48:41 +0800

    Add files via upload

---
+++ dist/data.js
@@ -0,0 +1,312 @@
+var teamSatMatch = "06/17(Sat.) UTC+8 17:00";
+var teamSunMatch = "06/18(Sun.) UTC+8 17:00";
+var teamWedMatch = "06/21(Wed.) UTC+8 18:00";
+
+var teamSatResult = "00/00(Sat.)";
+var teamSunResult = "00/00(Sun.)";
+var teamWedResult = "00/00(Wed.)";
+
+var indSatMatch = "06/17(Sat.) UTC+8 17:00";
+var indSunMatch = "06/18(Sun.) UTC+8 17:00";
+var indSatMatchTitle = "R.O.32 GROUP A";
+var indSunMatchTitle = "R.O.32 GROUP B";
+
+var indSatResult = "00/00(Sat.)";
+var indSunResult = "00/00(Sun..)";
+
+var Team = [
+  // Sat.
+  {
+    name:"ROX",
+    img:"Team/rox.png"
+  },
+  {
+    name:"ST",
+    img:"Team/st.png"
+  },
+  {
+    name:"KDF",
+    img:"Team/kdf.png"
+  },
+  {
+    name:"PO",
+    img:"Team/po.png"
+  },
+
+// Sun.
+  {
+    name:"LSB",
+    img:"Team/lsb.png"
+  },
+  {
+    name:"SST",
+    img:"Team/sst.png"
+  },
+  {
+    name:"AR",
+    img:"Team/ar.png"
+  },
+  {
+    name:"SRP",
+    img:"Team/srp.png"
+  },
+
+// Wed.
+  {
+    name:"SRP",
+    img:"Team/srp.png"
+  },
+  {
+    name:"KDF",
+    img:"Team/kdf.png"
+  },
+  {
+    name:"ST",
+    img:"Team/st.png"
+  },
+  {
+    name:"AR",
+    img:"Team/ar.png"
+  },
+  {
+    name:"PO",
+    img:"Team/po.png"
+  },
+  {
+    name:"LSB",
+    img:"Team/lsb.png"
+  }
+  
+];
+
+var TeamResult = [
+  // Sat.
+  {
+    name:"",
+    img:"",
+    score: "0",
+  },
+  {
+    name:"",
+    img:"",
+    score: "0",
+  },
+  {
+    name:"",
+    img:"",
+    score: "0",
+  },
+  {
+    name:"",
+    img:"",
+    score: "0",
+  },
+
+  // Sun.
+  {
+    name:"",
+    img:"",
+    score: "0",
+  },
+  {
+    name:"",
+    img:"",
+    score: "0",
+  },
+  {
+    name:"",
+    img:"",
+    score: "0",
+  },
+  {
+    name:"",
+    img:"",
+    score: "0",
+  },
+
+  // Wed.
+  {
+    name:"",
+    img:"",
+    score: "0",
+  },
+  {
+    name:"",
+    img:"",
+    score: "0",
+  },
+  {
+    name:"",
+    img:"",
+    score: "0",
+  },
+  {
+    name:"",
+    img:"",
+    score: "0",
+  },
+  {
+    name:"",
+    img:"",
+    score: "0",
+  },
+  {
+    name:"",
+    img:"",
+    score: "0",
+  }
+];
+
+var Player = [
+    {
+      name: "vaNilla",
+      img:"Player/vaNilla.png",
+    },
+    {
+      name: "Hong",
+      img:"Player/Hong.png",
+    },
+    {
+      name: "NEAL",
+      img:"Player/NEAL.png",
+    },
+    {
+      name: "UMaRu",
+      img:"Player/Unknown.png",
+    },
+    {
+      name: "Cool",
+      img:"Player/Cool.png",
+    },
+    {
+      name: "VEGA",
+      img:"Player/VEGA.png",
+    },
+    {
+      name: "Duzzi",
+      img:"Player/Duzzi.png",
+    },
+    {
+      name: "World",
+      img:"Player/World.png",
+    },
+
+
+    {
+      name: "SPEAR",
+      img:"Player/SPEAR.png",
+    },
+    {
+      name: "Most",
+      img:"Player/Most.png",
+    },
+    {
+      name: "ByungSu",
+      img:"Player/Speed.png",
+    },
+    {
+      name: "MINSIK",
+      img:"Player/MINSIK.png",
+    },
+    {
+      name: "Curren",
+      img:"Player/Unknown.png",
+    },
+    {
+      name: "HoJun",
+      img:"Player/HoJun.png",
+    },
+    {
+      name: "Hero",
+      img:"Player/Hero.png",
+    },
+    {
+      name: "Scarlet",
+      img:"Player/Scarlet.png",
+    }
+  ];
+
+var PlayerResult = [
+  {
+    name: "",
+    score: "",
+    img:"",
+  },
+  {
+    name: "",
+    score: "",
+    img:"",
+  },
+  {
+    name: "",
+    score: "",
+    img:"",
+  },
+  {
+    name: "",
+    score: "",
+    img:"",
+  },
+  {
+    name: "",
+    score: "",
+    img:"",
+  },
+  {
+    name: "",
+    score: "",
+    img:"",
+  },
+  {
+    name: "",
+    score: "",
+    img:"",
+  },
+  {
+    name: "",
+    score: "",
+    img:"",
+  },
+
+
+  {
+    name: "",
+    score: "",
+    img:"",
+  },
+  {
+    name: "",
+    score: "",
+    img:"",
+  },
+  {
+    name: "",
+    score: "",
+    img:"",
+  },
+  {
+    name: "",
+    score: "",
+    img:"",
+  },
+  {
+    name: "",
+    score: "",
+    img:"",
+  },
+  {
+    name: "",
+    score: "",
+    img:"",
+  },
+  {
+    name: "",
+    score: "",
+    img:"",
+  },
+  {
+    name: "",
+    score: "",
+    img:"",
+  }
+];
